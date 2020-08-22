34c7a2b32bb29fd795bca68f020f4ce07c6e53b9
star-centauri <limacosta25@hotmail.com>
2020-08-22 19:00:47 -0300

    first commit

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { response } = require('express');
+
+const Product = mongoose.model('Product');
+
+module.exports = {
+    async index(request, response) {
+        const { page = 1 } = request.query;
+        const products = await Product.paginate({}, { page, limit: 10 });
+        return response.json(products);
+    }
+    ,
+    async show(request, response) {
+        const product = await Product.findById(request.params.id);
+        return response.json(product);
+    }
+    ,
+    async store(request, response) {
+        const product = await Product.create(request.body);
+        return response.json(product);
+    }
+    ,
+    async update(request, response) {
+        const product = await Product.findByIdAndUpdate(request.params.id, request.body, { new: true });
+        return response.json(product);
+    }
+    ,
+    async destroy(request, response) {
+        await Product.findByIdAndRemove(request.params.id);
+        return response.send("Removido com sucesso!");
+    }
+}
